484c6fa127d98f6c08d7bcef968e07f4754e24dc
Harish Anantharajah <37521868+harishanan@users.noreply.github.com>
2020-10-15 21:19:55 -0700

    Updates

---
+++ precache-manifest.f65b1ba7cf73898288d3625bce0d2cbd.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aefca5e95bab2140ab952fc298104811",
+    "url": "/dansl-jardin-ordering/index.html"
+  },
+  {
+    "revision": "1a1fd092580dd2b0e76c",
+    "url": "/dansl-jardin-ordering/static/css/main.7aec1b14.chunk.css"
+  },
+  {
+    "revision": "9bf1c609888affab421f",
+    "url": "/dansl-jardin-ordering/static/js/2.9b6bec40.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dansl-jardin-ordering/static/js/2.9b6bec40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a1fd092580dd2b0e76c",
+    "url": "/dansl-jardin-ordering/static/js/main.995d9e7c.chunk.js"
+  },
+  {
+    "revision": "5af2a70621fd6fe4ec12",
+    "url": "/dansl-jardin-ordering/static/js/runtime-main.444447f6.js"
+  },
+  {
+    "revision": "86f002f3f3903fe7b45fd2883b8ec1b2",
+    "url": "/dansl-jardin-ordering/static/media/01_Logo-principal.86f002f3.png"
+  },
+  {
+    "revision": "86da952f8dafcc0f4ca3ffff6623019a",
+    "url": "/dansl-jardin-ordering/static/media/01_logo-horizontal - Copy.86da952f.png"
+  },
+  {
+    "revision": "ea26b3a4653b6c79d500116462c7c395",
+    "url": "/dansl-jardin-ordering/static/media/02-picto-artiste.ea26b3a4.png"
+  },
+  {
+    "revision": "c4f930420a8b59744fab0ec6e5537d4a",
+    "url": "/dansl-jardin-ordering/static/media/GRANDMERE2.c4f93042.png"
+  },
+  {
+    "revision": "cf9277dcadd1c65874736d760ac8415a",
+    "url": "/dansl-jardin-ordering/static/media/NeutraText Book.cf9277dc.otf"
+  },
+  {
+    "revision": "591c1e12b842f438fb20429634ea9626",
+    "url": "/dansl-jardin-ordering/static/media/NeutraTextDemi.591c1e12.otf"
+  },
+  {
+    "revision": "080fde2842ff4a22a9463880b0ee50b2",
+    "url": "/dansl-jardin-ordering/static/media/NeutraTextLight.080fde28.otf"
+  },
+  {
+    "revision": "e02331773c9f5d9c1582303965079775",
+    "url": "/dansl-jardin-ordering/static/media/accordeonisteblanc.e0233177.png"
+  },
+  {
+    "revision": "d3e44020330cc8b9c2b45b498d7863aa",
+    "url": "/dansl-jardin-ordering/static/media/duomusiciens.d3e44020.png"
+  },
+  {
+    "revision": "aef74f3a927ca32ac8433c46192ee478",
+    "url": "/dansl-jardin-ordering/static/media/institutionnew - Copy.aef74f3a.png"
+  },
+  {
+    "revision": "fee6b191e257b3540cf3b3fe651e903a",
+    "url": "/dansl-jardin-ordering/static/media/musiciennesolo.fee6b191.png"
+  },
+  {
+    "revision": "70c0a9cd27ee8d1035b5ad3a5a68068a",
+    "url": "/dansl-jardin-ordering/static/media/saxoblanc.70c0a9cd.png"
+  },
+  {
+    "revision": "730f9fa6c619619934b47abcc6617e14",
+    "url": "/dansl-jardin-ordering/static/media/tresorblanc.730f9fa6.png"
+  },
+  {
+    "revision": "a18ac9dc5eab5733750ee09ee132a1ef",
+    "url": "/dansl-jardin-ordering/static/media/violonsiteblanc.a18ac9dc.png"
+  }
+]);
